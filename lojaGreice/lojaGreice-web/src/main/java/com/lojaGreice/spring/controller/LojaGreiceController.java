69c55ab59fac8015f6d9732cf9b76bc5e5e8bc4a
Valdisnei <valdisnei.fajardo@gmail.com>
2015-08-22 20:34:11 -0300

    migrando para o spring Boot

--- lojaGreice/lojaGreice-web/src/main/java/com/lojaGreice/spring/controller/LojaGreiceController.java
+++ lojaGreice/lojaGreice-web/src/main/java/com/lojaGreice/spring/controller/LojaGreiceController.java
@@ -3,10 +3,10 @@ package com.lojaGreice.spring.controller;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lojaGreice.spring.dao.UserDAO;
 import com.lojaGreice.spring.model.User;
